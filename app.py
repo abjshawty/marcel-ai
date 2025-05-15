0d1570bb791225160d3910e61b80b1b41b2c2847
Timmy <kouadiobhegnino@gmail.com>
2025-05-15 00:06:33 +0000

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,10 @@ import functions
 
 app = Flask(__name__)
 
+@app.route('/hello', methods=['GET'])
+def hello():
+	return "<h1>🦆</h1>"
+
 @app.route('/q/<q>', methods=['GET', 'POST'])
 def question(q):
 	result = functions.vector_search(q)
@@ -15,3 +19,6 @@ def embed():
 @app.route('/embed/<file>', methods=['POST'])
 def embed_file(file):
 	return f"<p>{functions.embed_file(file).message}</p>"
+
+if __name__ == '__main__':
+	app.run('0.0.0.0', 5000, debug=False, use_reloader=False)
