caa6e1e713e51bc371e731aa39269269ebb1f906
Timmy <kouadiobhegnino@gmail.com>
2025-05-15 12:32:01 +0000

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -6,8 +6,9 @@ else
   exit 1
 fi
 echo "Killing the Marcel process."
-ps ax | grep marcel | grep -v grep | head -n 1 | awk '{print $1}' | xargs kill
+ps ax | grep gunicorn | grep marcel | grep -v grep | head -n 1 | awk '{print $1}' | xargs kill
 docker stop attu
 docker compose down
-docker system prune -a -y
-echo "You can safely delete the parent folder."
+docker system prune --all --force
+rm -rf -- "$(pwd -P)" && cd ..
+echo "Marcel was successfully removed from your system."
