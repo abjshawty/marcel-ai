ea046b91df87ac201eb0f7f376c7a92cd787df04
Timmy <kouadiobhegnino@gmail.com>
2025-05-15 00:28:32 +0000

    Update app.py

--- app.py
+++ app.py
@@ -20,5 +20,5 @@ def embed():
 def embed_file(file):
 	return f"<p>{functions.embed_file(file).message}</p>"
 
-if __name__ == '__main__':
-	app.run('0.0.0.0', 5000, debug=False, use_reloader=False)
+# if __name__ == '__main__':
+	# app.run('0.0.0.0', 5000, debug=False, use_reloader=False)
